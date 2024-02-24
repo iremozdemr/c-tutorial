650f93b9189939eb5c95a2c071179682d817814a
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-02-24 15:31:33 +0300

    added more tutorials

--- tutorial/tutorial15.c
+++ tutorial/tutorial15.c
@@ -2,19 +2,22 @@
 
 #define variable1 7
 
-const int variable2 = 6;
+enum variables {variable2=6};
+
+const int variable3 = 5;
 
 int main(){
-    const int variable3 = 5;
+    const int variable4 = 4;
 
     printf("variable1: %d \n",variable1);
     printf("variable2: %d \n",variable2);
     printf("variable3: %d \n",variable3);
+    printf("variable4: %d \n",variable4);
 
     return 0;
 }
 
-
 //constant variable'lar 2 şekilde tanımlanabilir
 //define kullanarak
+//enum kullanarak
 //const kullanarak
